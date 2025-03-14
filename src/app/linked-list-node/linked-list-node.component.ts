8f0c38e87aeb39bd2bad06c7bb07573db9929a19
otari nozadze <otikonozadze@gmail.com>
2025-03-14 14:08:13 +0400

    working on arrows

--- src/app/linked-list-node/linked-list-node.component.ts
+++ src/app/linked-list-node/linked-list-node.component.ts
@@ -6,9 +6,20 @@ import { Component, Input } from '@angular/core';
   styleUrl: './linked-list-node.component.scss',
 })
 export class LinkedListNodeComponent {
-  @Input() node: { x: number; y: number; val: number } = {
+  @Input() node: {
+    x: number;
+    y: number;
+    val: number;
+  } = {
     x: 0,
     y: 0,
     val: 0,
+  };
+
+  @Input() arrow: { x1: number; y1: number; x2: number; y2: number } = {
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 0,
   };
 }
--- src/app/list-cycle-detection/list-cycle-detection.component.ts
+++ src/app/list-cycle-detection/list-cycle-detection.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './list-cycle-detection.component.scss',
 })
 export class ListCycleDetectionComponent implements OnInit {
-  nodesToVis: { x: number; y: number; val: number }[] = [];
+  nodesToVis: {
+    x: number;
+    y: number;
+    val: number;
+  }[] = [];
+  arrowsToVis: { x1: number; y1: number; x2: number; y2: number }[] = [];
 
   nodes: number[] = [1, 2, 3, 4, 5, 6, 7];
 
@@ -19,11 +24,23 @@ export class ListCycleDetectionComponent implements OnInit {
 
     this.nodesToVis = this.nodes.map((_, i) => {
       const angle = -Math.PI + i * angleStep;
+      const nextIndex = (i + 1) % this.nodes.length; // Loop back to 0 after last node
+
       return {
         x: Math.cos(angle) * radius,
         y: Math.sin(angle) * radius,
         val: this.nodes[i],
       };
     });
+
+    for (let i = 0; i < this.nodesToVis.length - 1; i++) {
+      let arrows = {
+        x1: this.nodesToVis[i].x,
+        y1: this.nodesToVis[i].y,
+        x2: this.nodesToVis[i + 1].x,
+        y2: this.nodesToVis[i + 1].y,
+      };
+      this.arrowsToVis.push(arrows);
+    }
   }
 }
