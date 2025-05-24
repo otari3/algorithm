8848640541f7f8df34c9e7b84ea7f93960ec515b
otari nozadze <otikonozadze@gmail.com>
2025-05-24 04:18:30 +0400

    finiahws?

--- src/app/bzreaction/bzreaction/bzreaction.component.ts
+++ src/app/bzreaction/bzreaction/bzreaction.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HelperFucntionsService } from '../../shared/helper-fucntions.service';
+import { RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-bzreaction',
@@ -8,102 +9,265 @@ import { HelperFucntionsService } from '../../shared/helper-fucntions.service';
 })
 export class BzreactionComponent implements OnInit{
 constructor(private helperfn: HelperFucntionsService) {}
-colorPallet: Record<number, string> = {
-  0: '#FF0000',  1: '#FF7F00',  2: '#FFFF00',  3: '#7FFF00',  4: '#00FF00',
-  5: '#00FF7F',  6: '#00FFFF',  7: '#007FFF',  8: '#0000FF',  9: '#7F00FF',
- 10: '#FF00FF', 11: '#FF007F', 12: '#FF4040', 13: '#FF8040', 14: '#FFD700',
- 15: '#BFFF00', 16: '#40FF00', 17: '#00FF40', 18: '#00FFBF', 19: '#40FFFF',
- 20: '#4080FF', 21: '#0040FF', 22: '#8000FF', 23: '#BF00FF', 24: '#FF00BF',
- 25: '#FF1493', 26: '#FF69B4', 27: '#FFA07A', 28: '#FA8072', 29: '#FF8C00',
- 30: '#F0E68C', 31: '#ADFF2F', 32: '#32CD32', 33: '#00FA9A', 34: '#00CED1',
- 35: '#1E90FF', 36: '#4169E1', 37: '#6A5ACD', 38: '#8A2BE2', 39: '#9932CC',
- 40: '#C71585', 41: '#DB7093', 42: '#FFB6C1', 43: '#FFC0CB', 44: '#FFA500',
- 45: '#FFDAB9', 46: '#EEE8AA', 47: '#98FB98', 48: '#90EE90', 49: '#66CDAA',
- 50: '#48D1CC', 51: '#00BFFF', 52: '#87CEFA', 53: '#4682B4', 54: '#5F9EA0',
- 55: '#7B68EE', 56: '#BA55D3', 57: '#9370DB', 58: '#DA70D6', 59: '#FF69B4',
- 60: '#CD5C5C', 61: '#DC143C', 62: '#E9967A', 63: '#F4A460', 64: '#FFD700',
- 65: '#BDB76B', 66: '#9ACD32', 67: '#3CB371', 68: '#20B2AA', 69: '#5F9EA0',
- 70: '#1E90FF', 71: '#6495ED', 72: '#7B68EE', 73: '#8A2BE2', 74: '#9400D3',
- 75: '#FF1493', 76: '#FF6347', 77: '#FF4500', 78: '#FFA07A', 79: '#FF8C69',
- 80: '#DAA520', 81: '#9ACD32', 82: '#00FA9A', 83: '#20B2AA', 84: '#87CEEB',
- 85: '#4682B4', 86: '#6A5ACD', 87: '#9932CC', 88: '#DDA0DD', 89: '#D87093',
- 90: '#C71585', 91: '#FF1493', 92: '#E9967A', 93: '#F08080', 94: '#F5DEB3',
- 95: '#E6E6FA', 96: '#D8BFD8', 97: '#FFE4E1', 98: '#FFEFD5', 99: '#FFDAB9'
+
+colorPalette: Record<number, string> = {
+  0: '#f23d3d',
+  1: '#f2423d',
+  2: '#f2473d',
+  3: '#f24d3d',
+  4: '#f2523d',
+  5: '#f2583d',
+  6: '#f25d3d',
+  7: '#f2633d',
+  8: '#f2683d',
+  9: '#f26e3d',
+  10: '#f2733d',
+  11: '#f2793d',
+  12: '#f27e3d',
+  13: '#f2833d',
+  14: '#f2893d',
+  15: '#f28e3d',
+  16: '#f2943d',
+  17: '#f2993d',
+  18: '#f29f3d',
+  19: '#f2a43d',
+  20: '#f2aa3d',
+  21: '#f2af3d',
+  22: '#f2b43d',
+  23: '#f2ba3d',
+  24: '#f2bf3d',
+  25: '#f2c53d',
+  26: '#f2ca3d',
+  27: '#f2d03d',
+  28: '#f2d53d',
+  29: '#f2db3d',
+  30: '#f2e03d',
+  31: '#f2e63d',
+  32: '#f2eb3d',
+  33: '#f2f03d',
+  34: '#eff23d',
+  35: '#e9f23d',
+  36: '#e4f23d',
+  37: '#def23d',
+  38: '#d9f23d',
+  39: '#d3f23d',
+  40: '#cef23d',
+  41: '#c8f23d',
+  42: '#c3f23d',
+  43: '#bef23d',
+  44: '#b8f23d',
+  45: '#b3f23d',
+  46: '#adf23d',
+  47: '#a8f23d',
+  48: '#a2f23d',
+  49: '#9df23d',
+  50: '#97f23d',
+  51: '#92f23d',
+  52: '#8df23d',
+  53: '#87f23d',
+  54: '#82f23d',
+  55: '#7cf23d',
+  56: '#77f23d',
+  57: '#71f23d',
+  58: '#6cf23d',
+  59: '#66f23d',
+  60: '#61f23d',
+  61: '#5bf23d',
+  62: '#56f23d',
+  63: '#51f23d',
+  64: '#4bf23d',
+  65: '#46f23d',
+  66: '#40f23d',
+  67: '#3df23e',
+  68: '#3df244',
+  69: '#3df249',
+  70: '#3df24f',
+  71: '#3df254',
+  72: '#3df25a',
+  73: '#3df25f',
+  74: '#3df265',
+  75: '#3df26a',
+  76: '#3df26f',
+  77: '#3df275',
+  78: '#3df27a',
+  79: '#3df280',
+  80: '#3df285',
+  81: '#3df28b',
+  82: '#3df290',
+  83: '#3df296',
+  84: '#3df29b',
+  85: '#3df2a0',
+  86: '#3df2a6',
+  87: '#3df2ab',
+  88: '#3df2b1',
+  89: '#3df2b6',
+  90: '#3df2bc',
+  91: '#3df2c1',
+  92: '#3df2c7',
+  93: '#3df2cc',
+  94: '#3df2d2',
+  95: '#3df2d7',
+  96: '#3df2dc',
+  97: '#3df2e2',
+  98: '#3df2e7',
+  99: '#3df2ed',
+  100: '#3df2f2',
+  101: '#3dedf2',
+  102: '#3de7f2',
+  103: '#3de2f2',
+  104: '#3ddcf2',
+  105: '#3dd7f2',
+  106: '#3dd2f2',
+  107: '#3dccf2',
+  108: '#3dc7f2',
+  109: '#3dc1f2',
+  110: '#3dbcf2',
+  111: '#3db6f2',
+  112: '#3db1f2',
+  113: '#3dabf2',
+  114: '#3da6f2',
+  115: '#3da0f2',
+  116: '#3d9bf2',
+  117: '#3d96f2',
+  118: '#3d90f2',
+  119: '#3d8bf2',
+  120: '#3d85f2',
+  121: '#3d80f2',
+  122: '#3d7af2',
+  123: '#3d75f2',
+  124: '#3d6ff2',
+  125: '#3d6af2',
+  126: '#3d65f2',
+  127: '#3d5ff2',
+  128: '#3d5af2',
+  129: '#3d54f2',
+  130: '#3d4ff2',
+  131: '#3d49f2',
+  132: '#3d44f2',
+  133: '#3d3ef2',
+  134: '#403df2',
+  135: '#463df2',
+  136: '#4b3df2',
+  137: '#513df2',
+  138: '#563df2',
+  139: '#5b3df2',
+  140: '#613df2',
+  141: '#663df2',
+  142: '#6c3df2',
+  143: '#713df2',
+  144: '#773df2',
+  145: '#7c3df2',
+  146: '#823df2',
+  147: '#873df2',
+  148: '#8d3df2',
+  149: '#923df2',
+  150: '#973df2',
+  151: '#9d3df2',
+  152: '#a23df2',
+  153: '#a83df2',
+  154: '#ad3df2',
+  155: '#b33df2',
+  156: '#b83df2',
+  157: '#be3df2',
+  158: '#c33df2',
+  159: '#c83df2',
+  160: '#ce3df2',
+  161: '#d33df2',
+  162: '#d93df2',
+  163: '#de3df2',
+  164: '#e43df2',
+  165: '#e93df2',
+  166: '#ef3df2',
+  167: '#f23df0',
+  168: '#f23deb',
+  169: '#f23de6',
+  170: '#f23de0',
+  171: '#f23ddb',
+  172: '#f23dd5',
+  173: '#f23dd0',
+  174: '#f23dca',
+  175: '#f23dc5',
+  176: '#f23dbf',
+  177: '#f23dba',
+  178: '#f23db4',
+  179: '#f23daf',
+  180: '#f23daa',
+  181: '#f23da4',
+  182: '#f23d9f',
+  183: '#f23d99',
+  184: '#f23d94',
+  185: '#f23d8e',
+  186: '#f23d89',
+  187: '#f23d83',
+  188: '#f23d7e',
+  189: '#f23d79',
+  190: '#f23d73',
+  191: '#f23d6e',
+  192: '#f23d68',
+  193: '#f23d63',
+  194: '#f23d5d',
+  195: '#f23d58',
+  196: '#f23d52',
+  197: '#f23d4d',
+  198: '#f23d47',
+  199: '#f23d42',
 };
 
   matrix:number[][] = [];
   generateMatrix(){ 
-    for(let r = 0;r<80;r++){ 
+    for(let r = 0;r<50;r++){ 
       let row = []
-      for(let c=0;c<150;c++){ 
+      for(let c=0;c<118;c++){ 
         row.push(0);
       }
-      this.genrateRandomPatterns(row);
+      this.genrateRandomPatterns(row,118);
       this.matrix.push(row);
     }
   }
-  genrateRandomPatterns(row:number[]){  
-    let count = Math.floor(Math.random()*150);
+  genrateRandomPatterns(row:number[],g:number){  
+    let count = Math.floor(Math.random()*g);
     while(count>=0){  
-      let c = Math.floor(Math.random()*150);
-      let randomState = Math.floor(Math.random()*100)
+      let c = Math.floor(Math.random()*g);
+      let randomState = Math.floor(Math.random()*199)
+      let flip =  Math.floor(Math.random()*2);
       row[c] = randomState;
       count--;
     }
   }
-  applyRules(r:number,c:number,state:number[][],matrix:number[][],g:number,infected:number){ 
+  applyRules(r:number,c:number,state:number[][],matrix:number[][],k1:number,k2:number,g:number,q:number){ 
      let directons = [[1, 0],[-1, 0],[0, 1],[0, -1],[1, 1],[1, -1],[-1, 1],[-1, -1]];
-     let infectedAndIll = []
-     let allNb = []
-        if(matrix[r][c]===infected
-        ){ 
-          state.push([r,c,0])
-          return;
-        }
-
-     for(let dir of directons){ 
+     let ill = 0;
+     let infected = 0;
+     let s = matrix[r][c];
+      for(let dir of directons){  
         let dr = dir[0];
         let dc = dir[1];
-        if(Math.min(r+dr,c+dc)<0 || r+dr === matrix.length || c+dc===matrix[0].length){ 
+        if(Math.min(r+dr,c+dc)<0 || r+dr===matrix.length || c+dc===matrix[0].length){ 
           continue;
         }
-        if(matrix[r+dr][c+dc]>0 && matrix[r+dr][c+dc]<=infected){  
-          infectedAndIll.push(matrix[r+dr][c+dc]);
+        if(matrix[r+dr][c+dc]===q){ 
+          ill++;
+        }else if(matrix[r+dr][c+dc]!==0){ 
+          infected++;
         }
-        allNb.push(matrix[r+dr][c+dc]);
-     }
-     
-     if(matrix[r][c]>0&&matrix[r][c]<infected){  
-        if(infectedAndIll.length==0){    
-           state.push([r, c, Math.max(0, matrix[r][c] - 1)]);
-           return;
-        }
-      let sum = 0;
-      for(let n of allNb){  
-        sum+=n;
+        s+=matrix[r+dr][c+dc];
       }
-      let nextState = Math.floor(sum/allNb.length+g);
-      if(nextState>infected){  
-        state.push([r,c,infected]);
+      if(matrix[r][c]===0){ 
+        let k1State = Math.floor(infected/k1);
+        let k2State = Math.floor(ill/k2);
+        state.push([r,c,Math.min(k1State+k2State,q)]);
+      }else if(matrix[r][c]===q){ 
+        state.push([r,c,0]);
       }else{  
-        state.push([r,c,nextState])
+        let newState = Math.floor(s/(ill+infected+1))+g;
+        state.push([r,c,Math.min(newState,q)])
       }
-     }else if(matrix[r][c]===0){  
-        let sum = 0;
-        for(let n of infectedAndIll){ 
-          sum+=n;
-        }
-        if(infectedAndIll.length!==0){  
-          let nextState = Math.floor(sum/infectedAndIll.length);
-            state.push([r,c,nextState])
-        }
-     }
   }
 generateNextState() {
     let nextState: number[][] = [];
   for (let r = 0; r < this.matrix.length; r++) {
     for (let c = 0; c < this.matrix[0].length; c++) {
-      this.applyRules(r, c, nextState, this.matrix,1,99);
+      this.applyRules(r, c, nextState, this.matrix,2,3,70,199);
     }
   }
   
@@ -120,7 +284,7 @@ generateNextState() {
   ngOnInit(): void {
       this.generateMatrix();
       setInterval(()=>{ 
-        this.generateNextState()
-      },20)
+          this.generateNextState()
+      },100)
   }
 }
